3ff6b3a8f2eec3de6c9ff15033dde324c8bd9f2a
Jeky <Jeky.Cui@gmail.com>
2014-05-02 16:04:00 -0400

    fix bug in get_rank_in_record.py

--- get_rank_in_record.py
+++ get_rank_in_record.py
@@ -7,7 +7,7 @@ from collections import Counter
 STEP_IN_PIECE = 1000000
 RW_PIECE_NAME = data.RW_PATH + 'rw.%d.txt'
 
-tid = rank.getRank(1)[0]
+tid = '19058681'
 size = 5000
 
 steps = []
@@ -22,17 +22,19 @@ for i in range(rw.getRwFileCount()):
 
 	for l in rwpiece.xreadlines():
 		tid, tag = l.strip().split('\t')
-		steps.append(int(tid))
+		steps.append(tid)
 
 		if len(steps) == size:
 			rank = 0
+			count = 0
 			counter = Counter(steps)
-			for i, item in enumerate(counter.most_common()):
+			for r, item in enumerate(counter.most_common(100)):
 				if item[0] == tid:
-					rank = i + 1
+					rank = r + 1
+					count = item[1]
 					break
 
-			out.write('%d\n' % rank)
+			out.write('%d\t%d\n' % (rank, count))
 
 			steps = []
 
