1de6da8f21ed7e035a083b17ca1c150c823607ef
Jeky <Jeky.Cui@gmail.com>
2014-05-02 21:40:02 -0400

    fix bugs

--- get_rank_in_record.py
+++ get_rank_in_record.py
@@ -2,23 +2,24 @@ import rank
 import rw
 from collections import Counter
 
-fout = open('rank-1.txt')
+fout = open('rank-1.txt', 'w')
 steps = rw.readSteps()
 size = len(steps) / 10000
 tid = str(rank.getRank(1)[0])
 
-for i in range(100):
+for i in range(10000):
 	print 'size', size * i
 	counter = Counter([str(s) for s in steps[i * size: (i+1) * size]])
+	rank = 0
+	count = 0
+
 	for r, item in enumerate(counter.most_common()):
-		rank = 0
-		count = 0
 		if item[0] == tid:
 			rank = r + 1
 			count = item[1]
 			print 'find'
 			break
 		
-		fout.write('%d\t%d\n' % (rank, count))
+	fout.write('%d\t%d\n' % (rank, count))
 
 fout.close()
